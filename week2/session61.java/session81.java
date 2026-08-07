772e38636c8c65ce78045a0e9d78c040b2824ec9
hemanth9909 <hemanthkumar1548@gmail.com>
2026-08-07 10:06:35 +0530

    Add solution to find the highest altitude

---
+++ week2/session61.java/session81.java
@@ -0,0 +1,15 @@
+2. Find the Highest Altitud
+
+class Solution {
+    public int largestAltitude(int[] gain) {
+        int altitude = 0;
+        int maxAltitude = 0;
+
+        for (int g : gain) {
+            altitude += g;
+            maxAltitude = Math.max(maxAltitude, altitude);
+        }
+
+        return maxAltitude;
+    }
+}
