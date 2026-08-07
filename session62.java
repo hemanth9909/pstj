8629cc6697d0e2f6aa8eff54da33e41729952b65
hemanth9909 <hemanthkumar1548@gmail.com>
2026-08-07 09:37:17 +0530

    Clean up Session62.java by removing old code
    Removed unused code and comments from previous sessions.

---
+++ session62.java
@@ -0,0 +1,20 @@
+Week 2	
+
+Session 6
+
+
+2. Shuffle the Array 
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+        int[] ans = new int[2 * n];
+        int index = 0;
+
+        for (int i = 0; i < n; i++) {
+            ans[index++] = nums[i];
+            ans[index++] = nums[i + n];
+        }
+
+        return ans;
+    }
+}
+
