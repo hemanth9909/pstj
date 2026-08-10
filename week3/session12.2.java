c483be98f3e58d4f84ef0a1feebabd3dcdb00e68
hemanth9909 <hemanthkumar1548@gmail.com>
2026-08-10 09:59:35 +0530

    Add dayOfTheWeek method using LocalDate
    Implement method to determine the day of the week for a given date.

--- week3/session12.2.java
+++ week3/session12.2.java
@@ -1,1 +1,24 @@
+Day of Week:
+code:
+import java.time.LocalDate;
+import java.time.format.TextStyle;
+import java.util.Locale;
 
+class Solution {
+    public String dayOfTheWeek(int day, int month, int year) {
+        LocalDate date = LocalDate.of(year, month, day);
+
+        return date.getDayOfWeek()
+                   .getDisplayName(TextStyle.FULL, Locale.ENGLISH);
+    }
+}
+OUTPUT:
+Input
+day =
+31
+month =
+8
+year =
+2019
+Output
+"Saturday"
