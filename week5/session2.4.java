7455b49897cd8d2b523202f7d9011fc7666a52d1
hemanth9909 <hemanthkumar1548@gmail.com>
2026-08-24 09:55:30 +0530

    Create session2.4.java

---
+++ week5/session2.4.java
@@ -0,0 +1,41 @@
+maximum sumcircular subarrays
+  class Solution {
+    public int maxSubarraySumCircular(int[] nums) {
+
+        int totalSum = 0;
+
+        int currentMax = 0;
+        int maxSum = Integer.MIN_VALUE;
+
+        int currentMin = 0;
+        int minSum = Integer.MAX_VALUE;
+
+        for (int num : nums) {
+
+            // Maximum subarray sum
+            currentMax = Math.max(num, currentMax + num);
+            maxSum = Math.max(maxSum, currentMax);
+
+            // Minimum subarray sum
+            currentMin = Math.min(num, currentMin + num);
+            minSum = Math.min(minSum, currentMin);
+
+            totalSum += num;
+        }
+
+        // If all numbers are negative
+        if (maxSum < 0) {
+            return maxSum;
+        }
+
+        // Maximum circular sum
+        int circularSum = totalSum - minSum;
+
+        return Math.max(maxSum, circularSum);
+    }
+}
+input
+nums =
+[1,-2,3,-2]
+Output
+3
